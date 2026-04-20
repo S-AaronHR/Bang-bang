30ce0da553ed9a9b8934fe23c3a451a8fa5ffb01
AaronHdzRdz <shernandez114@alumnos.uaq.mx>
2026-04-19 20:43:46 -0600

    Refactor code structure for improved readability and maintainability

--- Bang.js
+++ Bang.js
@@ -27,7 +27,7 @@ function siguientePersonaje(){
         personajeActual = 1;
     }
     document.getElementById("personaje").src = "img/p"+personajeActual+".png"
-    var sfxclick = new Audio('sfx/Moneda.m4a');
+    var sfxclick = new Audio('sfx/click.m4a');
     sfxclick.play();
 }
 
@@ -37,7 +37,7 @@ function anteriorPersonaje(){
         personajeActual = 6;
     }
     document.getElementById("personaje").src = "img/p"+personajeActual+".png"
-    var sfxclick = new Audio('sfx/Moneda.m4a');
+    var sfxclick = new Audio('sfx/click.m4a');
     sfxclick.play();
 
 }
@@ -52,7 +52,7 @@ function personaje2(){
         function() {
             window.location.assign("Personaje2.html");
         },2000)
-    var sfxStart = new Audio('sfx/Moneda.m4a');
+    var sfxStart = new Audio('sfx/click.m4a');
     sfxStart.play();
 }
 
